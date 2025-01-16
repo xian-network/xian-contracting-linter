06b00aeef9f13ec2a5dcd249f8e0188582cbe644
kels7 <hiketabo@gmail.com>
2025-01-16 09:17:57 +0000

    fix: add chain_id to whitelist

--- xian_contracting_linter/__init__.py
+++ xian_contracting_linter/__init__.py
@@ -1,4 +1,4 @@
 from .main import lint_code
 
-__version__ = "0.2.13"
+__version__ = "0.2.14"
 __all__ = ["lint_code"]
--- xian_contracting_linter/main.py
+++ xian_contracting_linter/main.py
@@ -38,6 +38,7 @@ class Settings:
                 "decimal",
                 "Any",
                 "LogEvent",
+                "chain_id"
             }
         )
 
